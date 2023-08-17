de248419ad1b1520987b0f80e68c6cb5d1f4e3cb
Kavin S <132120544+KavinSelvaraju@users.noreply.github.com>
2023-08-17 09:10:05 +0530

    Create Integer.java

---
+++ INPUT_OUTPUT/Integer.java
@@ -0,0 +1,22 @@
+// Write a code to get the input in the given format and print the output in the given format
+
+// Input Description:
+// To take an integer value
+
+// Output Description:
+// Print the integer value
+
+// Sample Input :
+// 2
+// Sample Output :
+// 2
+
+
+import java.util.Scanner;
+public class Integer {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+    }
+}
