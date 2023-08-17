e48547d1d541cba384fd586f794a21c1d64bea18
Kavin S <132120544+KavinSelvaraju@users.noreply.github.com>
2023-08-17 09:18:48 +0530

    Create String2.java

---
+++ INPUT_OUTPUT/String2.java
@@ -0,0 +1,30 @@
+// Write a code to get the input in the given format and print the output in the given format
+
+// Input Description:
+// First-line indicates two integers separated by space. Second-line indicates three integers separated by space. Third-line indicates three integers separated by space
+
+// Output Description:
+// Print the input in the same format.
+
+// Sample Input :
+// 2 5
+// 2 5 6
+// 2 4 5
+// Sample Output :
+// 2 5
+// 2 5 6
+// 2 4 5
+
+
+import java.util.Scanner;
+public class String2 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      String s1=sc.nextLine();
+      String s2=sc.nextLine();
+      String s3=sc.nextLine();
+      System.out.println(s1);
+      System.out.println(s2);
+      System.out.println(s3);
+    }
+}
