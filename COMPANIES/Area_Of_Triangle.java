97f843ba02e2f50943a70804d2b19c39f05ce7a1
Kavin S <132120544+KavinSelvaraju@users.noreply.github.com>
2023-08-18 14:49:52 +0530

    Create Area_Of_Triangle.java

---
+++ COMPANIES/Area_Of_Triangle.java
@@ -0,0 +1,22 @@
+// Given base(B) and height(H) of a triangle find its area.
+// Input Size : N <= 1000000
+// Sample Testcase :
+// INPUT
+// 2 4
+// OUTPUT
+// 4
+
+
+
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+       double ans=0.5*l*b;
+        System.out.println(ans);
+    }
+}
